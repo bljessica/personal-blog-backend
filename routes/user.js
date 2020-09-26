07e589de5c07a14a0b59ce636a2d6e127a9ca255
bljessica <1194620082@qq.com>
2020-09-26 22:44:27 +0800

    add interface saveUserInfo

--- routes/user.js
+++ routes/user.js
@@ -4,9 +4,6 @@ const md5 = require('md5');
 const User = require('../models/user');
 const { respondMsg } = require('../utils/response');
 
-// router.get('/', (req, res) => {
-//     res.send(JSON.stringify({code: 0}));
-// })
 
 //用户注册
 router.post('/register', (req, res) => {
@@ -15,25 +12,21 @@ router.post('/register', (req, res) => {
         .then(user => {
             if(user) {
                 respondMsg(res, 1, '该手机号已被注册');
-                return;
             }
             else {
                 User.create({phone: obj.phone, nickname: obj.nickname, password: md5(obj.password)})
                     .then(resObj => {
                         respondMsg(res, 0, '注册成功');
-                        return;
                     })
                     .catch(err => {
                         respondMsg(res, 1, '注册失败');
                         console.log(err);
-                        return;
                     })
             }
         })
         .catch(err => {
             respondMsg(res, 1, '注册失败');
             console.log(err);
-            return;
         })
 })
 
@@ -44,28 +37,30 @@ router.post('/login', (req, res) => {
         .then(user => {
             if(!user) {
                 respondMsg(res, 1, '此手机号未注册');
-                return;
             }
             else {
                 if(md5(obj.password) == user.password) {
                     respondMsg(res, 0, '登陆成功');
-                    return;
                 }
                 else {
                     respondMsg(res, 1, '密码错误');
-                    return;
                 }
             }
+        })
+        .catch(err => {
+            respondMsg(res, 1, '查询失败');
+            console.log(err);
         })
 })
 
 //获取用户信息
-router.get('/getUser', (req, res) => {
+router.get('/getUserInfo', (req, res) => {
     let obj = req.query;
     User.findOne({phone: obj.phone})
         .then(user => {
             if(user) {
                 respondMsg(res, 0, '查询成功', {
+                    phone: user.phone,
                     nickname: user.nickname,
                     avatarUrl: user.avatarUrl
                 });
@@ -77,7 +72,32 @@ router.get('/getUser', (req, res) => {
         .catch(err => {
             respondMsg(res, 1, '查询失败');
             console.log(err);
-            return;
+        })
+})
+
+// 更新用户头像昵称
+router.post('/saveUserInfo', (req, res) => {
+    let obj = req.body;
+    User.findOne({phone: obj.phone})
+        .then(user => {
+            if(user) {
+                User.updateOne({phone: obj.phone}, {
+                    avatarUrl: obj.avatarUrl,
+                    nickname: obj.nickname
+                }).then(resObj => {
+                    respondMsg(res, 0, '更新成功');
+                }).catch(err => {
+                    respondMsg(res, 1, '查询失败');
+                    console.log(err);
+                })
+            }
+            else {
+                respondMsg(res, 1, '用户不存在');
+            }
+        })
+        .catch(err => {
+            respondMsg(res, 1, '查询失败');
+            console.log(err);
         })
 })
 
