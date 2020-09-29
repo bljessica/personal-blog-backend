60b786168fd4bc1d15bc916a2e4c88559d681ffb
bljessica <1194620082@qq.com>
2020-09-29 14:24:52 +0800

    add getComments interface

--- routes/comment.js
+++ routes/comment.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const moment = require('moment');
 const Comment = require('../models/comment');
+const { respondMsg } = require('../utils/response')
 const router = express.Router();
 
-router.post('/saveComment', (req, res) => {
+router.post('/saveComment', (req, res) => { 
     let obj = req.body;
+    
     Comment.create({
-        nickname: obj.nickname,
+        nickname: obj.nickname.length == 0? '路人': obj.nickname,
         email: obj.email,
         content: obj.content,
     }).then(() => {
@@ -15,4 +18,24 @@ router.post('/saveComment', (req, res) => {
         console.log(err);
         return;
     })
+});
+
+router.get('/getComments', (req, res) => {
+    Comment.find({}).then(comments => {
+        let data = [];
+        comments.forEach(item => {
+            data.push({
+                nickname: item.nickname,
+                content: item.content,
+                createTime: moment(item.createTime).format('YYYY-MM-DD HH:mm:ss')
+            });
+        })
+        respondMsg(res, 0, '查询成功', data);
+    }).catch(err => {
+        respondMsg(res, 1, '查询失败');
+        console.log(err);
+        return;
+    })
 })
+
+module.exports = router;
